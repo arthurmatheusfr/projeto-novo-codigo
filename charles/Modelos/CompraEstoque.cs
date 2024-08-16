d6435621101105c68bba22c003609ea8252edb97
arthur <arthurmatheusfreitas@gmail.com>
2024-08-16 09:00:47 -0300

    oi

---
+++ charles/Modelos/CompraEstoque.cs
@@ -0,0 +1,11 @@
+namespace charles;
+
+public class CompraEstoque
+{
+  string Tecido;
+  Int64 Id;
+  Int64 Qt;
+  string Local;
+
+
+}
---
+++ charles/Modelos/Status.cs
@@ -0,0 +1,11 @@
+namespace charles;
+
+public class Status
+{
+  string Correio;
+  Int64 Id;
+  Int64 Qt;
+  string Local;
+  string RastreamentoId;
+
+}
